805c3b14c42f3a19643c89a1d6ef53a18753582e
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-25 16:28:42 -0600

    added a switch statement for all id cases to trigger sound files for each click

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const clipA = new Audio('sources/a.mp3');
-const clipS = new Audio('sources/s.mp3');
-const clipD = new Audio('sources/d.mp3');
-const clipF = new Audio('sources/f.mp3');
-const clipJ = new Audio('sources/j.mp3');
-const clipK = new Audio('sources/k.mp3');
-const clipL = new Audio('sources/l.mp3');
-const clipRt = new Audio('sources/;.mp3');
-
 document.querySelectorAll('.img-thumbnail').forEach((item) => {
 	item.addEventListener('click', function() {
 		let buttonId = this.id;
-		console.log(buttonId);
+
+		switch (buttonId) {
+			case 'a':
+				const clipA = new Audio('sources/a.mp3');
+				clipA.play();
+				break;
+			case 's':
+				const clipS = new Audio('sources/s.mp3');
+				clipS.play();
+				break;
+			case 'd':
+				const clipD = new Audio('sources/d.mp3');
+				clipD.play();
+				break;
+			case 'f':
+				const clipF = new Audio('sources/f.mp3');
+				clipF.play();
+				break;
+			case 'j':
+				const clipJ = new Audio('sources/j.mp3');
+				clipJ.play();
+				break;
+			case 'k':
+				const clipK = new Audio('sources/k.mp3');
+				clipK.play();
+				break;
+			case 'l':
+				const clipL = new Audio('sources/l.mp3');
+				clipL.play();
+				break;
+			case ';':
+				const clipRt = new Audio('sources/;.mp3');
+				clipRt.play();
+				break;
+
+			default:
+				console.log(buttonId);
+				break;
+		}
 	});
 });
