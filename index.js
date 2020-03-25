9ac6fc7dd76ee5a232a74b9f410c590a97a467d8
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-25 16:15:38 -0600

    added ids to all images

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-document.querySelectorAll('.img-thumbnail').forEach((item) => {
-	item.addEventListener('click', handleClick);
-});
 const clipA = new Audio('sources/a.mp3');
+const clipS = new Audio('sources/s.mp3');
+const clipD = new Audio('sources/d.mp3');
+const clipF = new Audio('sources/f.mp3');
+const clipJ = new Audio('sources/j.mp3');
+const clipK = new Audio('sources/k.mp3');
+const clipL = new Audio('sources/l.mp3');
+const clipRt = new Audio('sources/;.mp3');
 
-function handleClick() {
-	clipA.play();
-}
+document.querySelectorAll('.img-thumbnail').forEach((item) => {
+	item.addEventListener('click', function() {
+		let buttonId = this.id;
+		console.log(buttonId);
+	});
+});
